206402482e53ee2bd5703465244ae1c3c5adc9f5
Dylan <dylan.zhang@icitymobile.com>
2019-07-30 15:58:08 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ApplicationRecord
   belongs_to :user
   validates :title , presence: true
 
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
