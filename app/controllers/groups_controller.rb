fd19d171b413f453f4a644754d9bf879862ed8ee
Dylan <dylan.zhang@icitymobile.com>
2019-07-30 13:56:17 +0800

    implement groups@update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,14 @@ class GroupsController < ApplicationController
   def edit
     @group=Group.find(params[:id])
   end
+
+  def update
+    @group=Group.find(params[:id])
+    @group.update_attributes(group_params)
+
+    redirect_to groups_path, notice: "Update Success"
+  end
+  
   private
     def group_params
       params.require(:group).permit(:title,:description)
