57631c891875bbeb339e23fd58854f751dff5c8f
Dylan <dylan.zhang@icitymobile.com>
2019-07-30 14:12:56 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,9 +26,11 @@ class GroupsController < ApplicationController
 
   def update
     @group=Group.find(params[:id])
-    @group.update_attributes(group_params)
-
-    redirect_to groups_path, notice: "Update Success"
+    if @group.update_attributes(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else 
+      render :edit
+    end
   end
 
   def destroy
